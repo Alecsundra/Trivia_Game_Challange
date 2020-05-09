544f3b59facfff5044125dc3f46781684e01221f
Sandra <alecsandra.serediuc@gmail.com>
2020-05-09 11:38:34 +0200

    installed axios; api call, object data in console

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React , { Component } from 'react';
+import axios from "axios"
 import './App.css';
 
-function App() {
+class App extends Component {
+  state={
+    data:[]
+  }
+  // api call to get the object with the questions
+  async componentDidMount(){
+    const data = await axios(`https://opentdb.com/api.php?amount=10&category=29&type=multiple`)
+    
+        this.setState({
+          data:data
+          
+         })
+console.log(data)
+  }
+render (){
   return (
     <div className="App">
      
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      
         <p>
          Home
         </p>
       
     </div>
   );
+}
 }
 
 export default App;
